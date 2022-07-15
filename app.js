398ff8d5a1a0c4b8350254c87c4abb7e78b7cf25
samucorea <samuel.alejandro100@hotmail.com>
2022-07-14 21:34:54 -0400

    feat:fix port env variable

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const vowelCounter = require('./VowelCounter');
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/vowelCounter/:number', (req, res) => {
 const result = vowelCounter(req.params.number)
